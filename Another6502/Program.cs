655654059f5478adf5a83bdddda2c7bfe7d9b2d6
Moiv <moiv@moiv.net>
2021-01-30 08:32:27 +1000

    Add project files.

---
+++ Another6502/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Another6502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
